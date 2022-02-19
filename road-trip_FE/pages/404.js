311cbe67c0e10e57efc0f451225e1b27e44fba2c
YoonMinSeok <81290520+YoonMinSeok@users.noreply.github.com>
2022-02-19 15:37:05 +0900

    404페이지 추가 완료

---
+++ road-trip_FE/pages/404.js
@@ -0,0 +1,12 @@
+// import Header from 'header.js';
+// import Footer from 'footer.js';
+
+export default function Custom404() {
+  return (
+    <>
+      {/* <Header /> */}
+      <h1>404 Error - Page Not Found</h1>
+      {/* <Footer /> */}
+    </>
+  );
+}
